096799593516ee5aeeff90dc492947a018619ec7
unknown <qwertyx7xx@gmail.com>
2020-03-06 01:08:30 +0100

    ii

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,8 +1,17 @@
 var resultsID = [];
 var plots = [];
 var ratings = [];
+isResult = Boolean(true);
+
 $(document).ready(function(){
     $('#movie-search-form').on('submit', function(e) { 
+
+        $(".search-button").click();    //it just slides the navbar...
+console.log(isResult)
+        if(!isResult) {
+            $('.noresults').remove();
+            isResult = true;
+        }
 
         $('#ddt23').remove();
         $('.lio').remove();
@@ -17,7 +26,8 @@ $(document).ready(function(){
                         console.log(result);
   
                          if(result.Response == "False"){
-                            $('#dd1').append('<br></br><br></br>Sorry, no movies matching search query<br></br><br></br>');
+                            $('#dd1').append('<a class="noresults"><br></br><br></br>Sorry, no movies matching search query<br></br><br></br></a>');
+                            isResult = false;
                          }
         
 
@@ -26,10 +36,11 @@ $(document).ready(function(){
                             
                            // $('#dd1').append(`<li><a href=#}>${result.Search[i].Title}</a><br><img src=${result.Search[i].Poster}></li><br><br>`);
                            
-                           $('#dd1').append(`<li class="lio" id='f${i}'><br><a class="titleS" onclick="getPlot(${i})">${result.Search[i].Title} (${result.Search[i].Year})<br><img src='star.png'><p id='rt${i}'></p></a><img class="poster" src=${result.Search[i].Poster}><p class='pf' id='pf${i}'>.</p><br></li>`);
+                           $('#dd1').append(`<li class="lio" id='f${i}'><br><a class="titleS" onclick="getPlot(${i})">${result.Search[i].Title} (${result.Search[i].Year})<br></a><img class="poster" src=${result.Search[i].Poster}><br><p class='pf' id='pf${i}'>.</p><br></li>`);
                            resultsID[i]=result.Search[i].imdbID;console.log(ratings[i])
-                           getRating(i);
-                            //$('#dd1').append(`<a href=#}>${result.Search[i].Title}</a><br><img src=${result.Search[i].Poster}>`);
+                           
+                            // $('#dd1').append(`<li class="lio" id='f${i}'><br><a class="titleS" onclick="getPlot(${i})">${result.Search[i].Title} (${result.Search[i].Year})<br><img src='star.png'><p id='rt${i}'></p></a><img class="poster" src=${result.Search[i].Poster}><br><p class='pf' id='pf${i}'>.</p><br></li>`);
+                            //   getRating(i);
                         }
 
                         
@@ -44,10 +55,11 @@ $(document).ready(function(){
 
             function getPlot(i){
                     if($('#pf'+i).html()=='.'){                                                                 //if description not toggled...
-                     $.get('https://www.omdbapi.com?apikey=fc4d8629&i='+resultsID[i], function(result) {
+                     $.get('https://www.omdbapi.com?apikey=fc4d8629&i='+resultsID[i]+'&plot=full', function(result) {
                     console.log(result.Plot) 
-                     $('#pf'+i).html(result.Plot);
-                    
+                     $('#pf'+i).html(result.Plot+'<br><br><p id="rt${i}">'+result.imdbRating+'/10 <img src="star.png"></p><a href=http://imdb.com/title/'+resultsID[i]+'><img src=assets/ico/imdb.png></a>');
+                    //appends plot + link to imdb 
+      
                     });
                     }
                      else{
